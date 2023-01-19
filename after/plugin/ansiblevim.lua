572589f58fe8cc61264500f0b4e61c2ac6bdec31
Ben Watson <bwatson1979@gmail.com>
2023-01-19 10:50:47 -0600

    moar highlighting (that doesn't actually work)

--- after/plugin/ansiblevim.lua
+++ after/plugin/ansiblevim.lua
@@ -1,6 +1,6 @@
 vim.g.ansible_unindent_after_newline = 1
 --vim.g.ansible_yamlKeyName = 'yamlKey'
-vim.g.ansible_attribute_highlight = "n"
+--vim.g.ansible_attribute_highlight = "n"
 vim.g.ansible_name_highlight = 'd'
 vim.g.ansible_extra_keywords_highlight = 1
 vim.g.ansible_extra_keywords_highlight_group = 'Structure'
