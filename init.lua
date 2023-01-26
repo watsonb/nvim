26298387e567f03baa657893e90a561eb0f8691e
Ben Watson <bwatson1979@gmail.com>
2023-01-26 08:01:46 -0600

    add lualine plugin

--- init.lua
+++ init.lua
@@ -132,5 +132,10 @@ return require('packer').startup(function(use)
 
   use('preservim/nerdtree')
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
 end)
 
