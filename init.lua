af82b06bb818f74261701c8bdbf703ef301a75e1
Ben Watson <bwatson1979@gmail.com>
2023-04-03 14:52:13 -0500

    add neotree, vim-devicons

--- init.lua
+++ init.lua
@@ -133,6 +133,21 @@ return require('packer').startup(function(use)
   use('preservim/nerdtree')
 
   use {
+    "nvim-neo-tree/neo-tree.nvim",
+      branch = "v2.x",
+      requires = { 
+        "nvim-lua/plenary.nvim",
+        -- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+      },
+      -- config = function()
+      --   -- Unless you are still migrating, remove the deprecated commands from v1.x
+      --   vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+  }
+
+  use('ryanoasis/vim-devicons')
+
+  use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
