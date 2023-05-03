d77003c384f1311a7eceb99de5f1e574b64a521f
Ben Watson <Benjamin.Watson@kiewit.com>
2023-05-03 07:19:34 -0500

    treesitter to 0.1.1, add git-blame

--- init.lua
+++ init.lua
@@ -55,7 +55,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
@@ -191,6 +191,7 @@ return require('packer').startup(function(use)
   }
 
   use ('HiPhish/nvim-ts-rainbow2')
+  use ('f-person/git-blame.nvim')
 
 end)
 
