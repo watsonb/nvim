10a191296db8b650c0c94086c4d6ce617f291aa1
Ben Watson <Benjamin.Watson@kiewit.com>
2023-06-21 09:58:39 -0500

    refactor lazygit keymaps
    moved keymaps to own plugin lua file.  figured out bug in copy/pasta example

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,6 @@
+
+local Util = require("lazy.util")
+
+-- lazygit
+vim.keymap.set("n", "<leader>gg", function() Util.float_term({ "lazygit" }, { cwd = Util.get_root(), esc_esc = false, ctrl_hjkl = false }) end, { desc = "Lazygit (root dir)" })
+vim.keymap.set("n", "<leader>gG", function() Util.float_term({ "lazygit" }, {esc_esc = false, ctrl_hjkl = false}) end, { desc = "Lazygit (cwd)" })
--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -31,9 +31,6 @@ vim.keymap.set("v", ">", ">gv")
 -- lazy
 vim.keymap.set("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
 
--- lazygit
-vim.keymap.set("n", "<leader>gg", function() Util.float_term({ "lazygit" }, { cwd = Util.get_root(), esc_esc = false, ctrl_hjkl = false }) end, { desc = "Lazygit (root dir)" })
-vim.keymap.set("n", "<leader>gG", function() Util.float_term({ "lazygit" }, {esc_esc = false, ctrl_hjkl = false}) end, { desc = "Lazygit (cwd)" })
 
 -- quit
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
