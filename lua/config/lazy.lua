d39bd13b9d702c1fa03292eda1ed81711b425de1
Ben Watson <Benjamin.Watson@kiewit.com>
2023-12-06 07:21:03 -0600

    change default loaded color scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "onedark_dark" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
