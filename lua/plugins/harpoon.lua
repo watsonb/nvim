d12bf000e4670bf5aa4c00a79d019973b2576876
Ben Watson <Benjamin.Watson@kiewit.com>
2024-01-17 07:07:53 -0600

    fix up harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,6 +1,12 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "master",
+  -- event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  opts = { menu = { width = 120 } },
+  -- keys = {
+  --   { "<leader>hh", require("harpoon.ui").toggle_quick_menu), desc = "open Cheatsheet" },
+  -- },
 }
