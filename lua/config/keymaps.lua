c54d4517c0221594240ce9b5b3e0766a432179c5
Ben Watson <Benjamin.Watson@kiewit.com>
2024-01-17 07:09:17 -0600

    better harpoon keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -51,25 +51,48 @@ vim.keymap.set(
   { buffer = 0, desc = "escape text entry in terminal and ready to move to window" }
 )
 
-
 -- Trouble
 -- Lua
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { desc = "Toggle Trouble" },
+vim.keymap.set(
+  "n",
+  "<leader>xx",
+  "<cmd>TroubleToggle<cr>",
+  { desc = "Toggle Trouble" },
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { desc = "Toggle Trouble (workspace)" },
+vim.keymap.set(
+  "n",
+  "<leader>xw",
+  "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  { desc = "Toggle Trouble (workspace)" },
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { desc = "Toggle Trouble (document)" },
+vim.keymap.set(
+  "n",
+  "<leader>xd",
+  "<cmd>TroubleToggle document_diagnostics<cr>",
+  { desc = "Toggle Trouble (document)" },
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { desc = "Toggle Trouble location list" },
+vim.keymap.set(
+  "n",
+  "<leader>xl",
+  "<cmd>TroubleToggle loclist<cr>",
+  { desc = "Toggle Trouble location list" },
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { desc = "Toggle Trouble quickfix" },
+vim.keymap.set(
+  "n",
+  "<leader>xq",
+  "<cmd>TroubleToggle quickfix<cr>",
+  { desc = "Toggle Trouble quickfix" },
   { silent = true, noremap = true }
 )
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { desc = "Toggle Trouble LSP References" },
+vim.keymap.set(
+  "n",
+  "gR",
+  "<cmd>TroubleToggle lsp_references<cr>",
+  { desc = "Toggle Trouble LSP References" },
   { silent = true, noremap = true }
 )
 
@@ -78,21 +101,58 @@ vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { desc = "Tog
 -- resizing splits
 -- these keymaps will also accept a range,
 -- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left, { desc = "resize left" })
-vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down, { desc = "resize down" })
-vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up, { desc = "resize up" })
-vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right, { desc = "resize right" })
+vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left, { desc = "resize left" })
+vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down, { desc = "resize down" })
+vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up, { desc = "resize up" })
+vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right, { desc = "resize right" })
 -- moving between splits
-vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left, { desc = "move cursor left" })
-vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down, { desc = "move cursor down" })
-vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up, { desc = "move cursor up" })
-vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right, { desc = "move cursor right" })
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "move cursor left" })
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "move cursor down" })
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "move cursor up" })
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "move cursor right" })
 -- swapping buffers between windows
-vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left, { desc = "swap buffer left" })
-vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down, { desc = "swap buffer down" })
-vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up, { desc = "swap buffer up" })
-vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right, { desc = "swap buffer right" })
+vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left, { desc = "swap buffer left" })
+vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down, { desc = "swap buffer down" })
+vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up, { desc = "swap buffer up" })
+vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right, { desc = "swap buffer right" })
 
 -- venv select
 vim.keymap.set("n", "<leader>vs", "<cmd>VenvSelect<cr>", { desc = "[v]env [s]elect" })
 vim.keymap.set("n", "<leader>vc", "<cmd>VenvSelectCached<cr>", { desc = "[v]envselect [c]ached" })
+
+-- Harpoon
+vim.keymap.set("n", "<leader>ha", require("harpoon.mark").add_file, { desc = "Harpoon add buffer" })
+vim.keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu, { desc = "Harpoon open UI" })
+vim.keymap.set("n", "<leader>h1", function()
+  require("harpoon.ui").nav_file(1)
+end, { desc = "Harpoon goto file 1" })
+vim.keymap.set("n", "<leader>h2", function()
+  require("harpoon.ui").nav_file(2)
+end, { desc = "Harpoon goto file 2" })
+vim.keymap.set("n", "<leader>h3", function()
+  require("harpoon.ui").nav_file(3)
+end, { desc = "Harpoon goto file 3" })
+vim.keymap.set("n", "<leader>h4", function()
+  require("harpoon.ui").nav_file(4)
+end, { desc = "Harpoon goto file 4" })
+vim.keymap.set("n", "<leader>h5", function()
+  require("harpoon.ui").nav_file(5)
+end, { desc = "Harpoon goto file 5" })
+vim.keymap.set("n", "<leader>h6", function()
+  require("harpoon.ui").nav_file(6)
+end, { desc = "Harpoon goto file 6" })
+vim.keymap.set("n", "<leader>h7", function()
+  require("harpoon.ui").nav_file(7)
+end, { desc = "Harpoon goto file 7" })
+vim.keymap.set("n", "<leader>h8", function()
+  require("harpoon.ui").nav_file(8)
+end, { desc = "Harpoon goto file 8" })
+vim.keymap.set("n", "<leader>h9", function()
+  require("harpoon.ui").nav_file(9)
+end, { desc = "Harpoon goto file 9" })
+vim.keymap.set("n", "<leader>hn", function()
+  require("harpoon.ui").nav_next()
+end, { desc = "Harpoon goto next file" })
+vim.keymap.set("n", "<leader>hp", function()
+  require("harpoon.ui").nav_prev()
+end, { desc = "Harpoon goto prev file" })
