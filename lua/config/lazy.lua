b6893e52f35bd80d80f8d3c3f50fce792dad6958
Ben Watson <benjamin.watson@kiewit.com>
2024-05-15 10:03:22 -0500

    bug: disable mini-starter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.lang.python" },
