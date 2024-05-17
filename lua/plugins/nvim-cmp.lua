e6210ba718cc167bd042d4b6a107e781ff467250
Ben Watson <benjamin.watson@kiewit.com>
2024-05-17 08:43:45 -0500

    fix: change cmp mapping complete to ctrl + h

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -23,7 +23,8 @@ return {
         ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        -- ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-h>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         -- not sure how I feel about pressing enter to confirm, seems to always get in my way
         ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
