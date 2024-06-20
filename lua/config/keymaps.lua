6819d4881161945d8b7e290c94debd3215d40669
Ben Watson <benjamin.watson@kiewit.com>
2024-06-20 07:42:39 -0500

    fix trouble keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -56,45 +56,45 @@ vim.keymap.set(
 vim.keymap.set(
   "n",
   "<leader>xx",
-  "<cmd>TroubleToggle<cr>",
-  { desc = "Toggle Trouble" },
+  "<cmd>Trouble diagnostics toggle<cr>",
+  { desc = "Toggle Trouble diagnostics" },
   { silent = true, noremap = true }
 )
-vim.keymap.set(
-  "n",
-  "<leader>xw",
-  "<cmd>TroubleToggle workspace_diagnostics<cr>",
-  { desc = "Toggle Trouble (workspace)" },
-  { silent = true, noremap = true }
-)
-vim.keymap.set(
-  "n",
-  "<leader>xd",
-  "<cmd>TroubleToggle document_diagnostics<cr>",
-  { desc = "Toggle Trouble (document)" },
-  { silent = true, noremap = true }
-)
-vim.keymap.set(
-  "n",
-  "<leader>xl",
-  "<cmd>TroubleToggle loclist<cr>",
-  { desc = "Toggle Trouble location list" },
-  { silent = true, noremap = true }
-)
-vim.keymap.set(
-  "n",
-  "<leader>xq",
-  "<cmd>TroubleToggle quickfix<cr>",
-  { desc = "Toggle Trouble quickfix" },
-  { silent = true, noremap = true }
-)
-vim.keymap.set(
-  "n",
-  "gR",
-  "<cmd>TroubleToggle lsp_references<cr>",
-  { desc = "Toggle Trouble LSP References" },
-  { silent = true, noremap = true }
-)
+-- vim.keymap.set(
+--   "n",
+--   "<leader>xw",
+--   "<cmd>TroubleToggle workspace_diagnostics<cr>",
+--   { desc = "Toggle Trouble (workspace)" },
+--   { silent = true, noremap = true }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<leader>xd",
+--   "<cmd>TroubleToggle document_diagnostics<cr>",
+--   { desc = "Toggle Trouble (document)" },
+--   { silent = true, noremap = true }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<leader>xl",
+--   "<cmd>TroubleToggle loclist<cr>",
+--   { desc = "Toggle Trouble location list" },
+--   { silent = true, noremap = true }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "<leader>xq",
+--   "<cmd>TroubleToggle quickfix<cr>",
+--   { desc = "Toggle Trouble quickfix" },
+--   { silent = true, noremap = true }
+-- )
+-- vim.keymap.set(
+--   "n",
+--   "gR",
+--   "<cmd>TroubleToggle lsp_references<cr>",
+--   { desc = "Toggle Trouble LSP References" },
+--   { silent = true, noremap = true }
+-- )
 
 -- smart splits
 -- recommended mappings
