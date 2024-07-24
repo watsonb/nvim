1db396ee9b71952b54821a153e9a1478ebe3247a
Ben Watson <benjamin.watson@kiewit.com>
2024-07-24 10:01:24 -0500

    chore: leverage more lazyvim extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,13 +12,18 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.editor.mini-diff" },
+    { import = "lazyvim.plugins.extras.lang.ansible" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    { import = "lazyvim.plugins.extras.util.rest" },
+    { import = "lazyvim.plugins.extras.util.startuptime" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,5 +1,6 @@
 return {
   "rest-nvim/rest.nvim",
+  enabled = false,
   ft = "http",
   dependencies = { "luarocks.nvim" },
   config = function()
