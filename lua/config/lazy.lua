dc262cfeafa5a57f83ea13c3c93b36358da58684
Ben Watson <benjamin.watson@kiewit.com>
2024-12-05 10:41:46 -0600

    fix: disable tohtml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -48,7 +48,7 @@ require("lazy").setup({
         -- "matchparen",
         -- "netrwPlugin",
         "tarPlugin",
-        "tohtml",
+        -- "tohtml",
         "tutor",
         "zipPlugin",
       },
