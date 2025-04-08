78a855a8fbfa9d941893d5c1fa9ae8cb93675d59
Ben Watson <benjamin.watson@kiewit.com>
2025-04-08 06:49:43 -0500

    feat: quick Google command shortcut

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,10 @@
 --
 vim.cmd("autocmd BufRead,BufNewFile *.{yaml,yml} set filetype=yaml.ansible")
 vim.cmd("autocmd FileType yaml.ansible setlocal ts=2 sts=2 sw=2 expandtab indentkeys-=0# indentkeys-=<:>")
+
+vim.api.nvim_create_user_command("Google", function(o)
+  -- local escaped = require('socket.url').escape(o.args)
+  local escaped = vim.uri_encode(o.args)
+  local url = ("https://www.google.com/search?q=%s"):format(escaped)
+  vim.ui.open(url)
+end, { nargs = 1, desc = "just google it" })
