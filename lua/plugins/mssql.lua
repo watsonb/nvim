7fa2970be4c0645024a6b7f37b7ea8c99f91587e
Ben Watson <benjamin.watson@kiewit.com>
2025-06-20 08:17:14 -0500

    fix: mssql loading issue
    https://github.com/Kurren123/mssql.nvim/issues/60#issuecomment-2923038112

--- lua/plugins/mssql.lua
+++ lua/plugins/mssql.lua
@@ -1,10 +1,15 @@
 return {
   "Kurren123/mssql.nvim",
-  opts = {},
   -- optional. You also need to call set_keymaps (see below)
   dependencies = { "folke/which-key.nvim" },
   config = function()
-    require("mssql").setup()
-    require("mssql").set_keymaps("<leader>D")
+    -- require("mssql").setup()
+    -- require("mssql").set_keymaps("<leader>D")
+    local mssql = require("mssql")
+    mssql.setup({
+      max_column_width = 1000,
+    }, function()
+      mssql.set_keymaps("<leader>D")
+    end)
   end,
 }
