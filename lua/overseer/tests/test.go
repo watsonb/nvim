36517ae47bec4002f5ce8e7a7fb7ebc47484a0a9
Ben Watson <benjamin.watson@kiewit.com>
2025-11-12 12:05:58 -0600

    feat: trying out overseer

---
+++ lua/overseer/tests/test.go
@@ -0,0 +1,8 @@
+// test.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
---
+++ lua/plugins/overseer.nvim.lua
@@ -0,0 +1,7 @@
+-- Plugin: stevearc/overseer.nvim
+-- Installed via store.nvim
+
+return {
+    "stevearc/overseer.nvim",
+    opts = {}
+}
