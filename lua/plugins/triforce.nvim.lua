5f2dfe7594158c3047761e93917713938e599777
Ben Watson <benjamin.watson@kiewit.com>
2026-02-05 12:20:47 -0600

    feat: add triforce for some fun

---
+++ lua/plugins/triforce.nvim.lua
@@ -0,0 +1,19 @@
+-- Plugin: gisketch/triforce.nvim
+-- Installed via store.nvim
+
+return {
+    "gisketch/triforce.nvim",
+    dependencies = {
+        "nvzone/volt"
+    },
+    config = function()
+        require("triforce").setup(
+            {
+                -- Optional: Add your configuration here
+                keymap = {
+                    show_profile = "<leader>tp" -- Open profile with <leader>tp
+                }
+            }
+        )
+    end
+}
