62af44168fe39299b8a99849ef2a0366d681e58c
Ben Watson <benjamin.watson@kiewit.com>
2026-02-05 12:21:15 -0600

    vibe: gemini trying to help me solve some LSP issue
    Error  12:12:24 PM msg_show.lua_error vim.schedule callback:
    /home/ben/share/nvim/runtime/lua/vim/lsp/client.lua:570: RPC[Error]
    code_name = InternalError, message = "Request initialize failed with
    message: [UriError]: Scheme is missing: {scheme: \"\", authority: \"\",
    path: \"null\", query: \"\", fragment: \"\"}"stack traceback:[C]: in
    function
    'assert'/home/ben/share/nvim/runtime/lua/vim/lsp/client.lua:570: in
    function ''vim/_core/editor.lua: in function <vim/_core/editor.lua:0>

---
+++ lua/plugins/ansible_lsp.lua
@@ -0,0 +1,48 @@
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = {
+          -- make sure mason installs this for you
+          keys = {
+            -- `gopls` only supports `textDocument/prepareCallHierarchy` if
+            -- `callHierarchy` config is enabled. Don't add keybinds to this
+            -- client unless you explicitly enable this config.
+            -- This is only an example
+            -- { "<leader>gC", "<cmd>lua vim.lsp.buf.prepare_call_hierarchy()<cr>", desc = "Call Hierarchy" },
+          },
+          settings = {
+            yaml = {
+              schemas = {
+                ["https://raw.githubusercontent.com/ansible/ansible-lint/main/schemas/ansible.json"] = "ansible/*.y*ml",
+                ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible/ansible-playbook.json"] = "*playbook*.yml",
+                ["https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible/ansible.json"] = "ansible.y*ml",
+                -- You can add more specific schemas here if needed.
+              },
+              customTags = {
+                "!ENV scalar",
+                "!include_dir_named mapping",
+                "!include_dir_merge_named mapping",
+                "!include_dir_list sequence",
+                "!include_dir_merge_list sequence",
+                "!vault",
+                "!encrypted",
+                "!secret",
+                "!unsafe",
+              },
+            },
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "ansible.cfg",
+              "requirements.yml",
+              ".git"
+            )(fname) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+        },
+      },
+    },
+  },
+}
