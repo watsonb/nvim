5da53af9b149b28b33e11d28cccc0994527c881f
Ben Watson <benjamin.watson@kiewit.com>
2026-03-18 06:22:05 -0500

    fix: make triforce work for Ansible

--- lua/plugins/triforce.nvim.lua
+++ lua/plugins/triforce.nvim.lua
@@ -9,13 +9,12 @@ return {
   config = function()
     require("triforce").setup({
       -- Optional: Add your configuration here
-      -- custom_languages = {
-      --     yamlansible = { name = 'Ansible', icon = '' }, -- nf-seti-yaml
-      -- },
+      custom_languages = {
+        ["yaml.ansible"] = { name = "Ansible", icon = "" }, -- nf-seti-yaml
+      },
       keymap = {
         show_profile = "<leader>tp", -- Open profile with <leader>tp
       },
     })
   end,
 }
-
