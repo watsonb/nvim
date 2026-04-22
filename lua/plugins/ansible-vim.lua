72458ff510bafcdb56dde6fc9d8d6fee4d370104
Ben Watson <benjamin.watson@kiewit.com>
2026-04-22 09:28:51 -0500

    chore: pin ansible-vim version
    moving > 3.4 is causing highlight pain that I don't want to figure out
    now

--- lua/plugins/ansible-vim.lua
+++ lua/plugins/ansible-vim.lua
@@ -1,3 +1,7 @@
 return {
   "pearofducks/ansible-vim",
+  -- TODO: figure out what the author did with version >= 4.x and why the syntax
+  -- highlighting isn't what _I_ like with respect to the ansible vs yaml.ansible
+  -- filetypes.
+  version = "3.4",
 }
